7da0a98e0e1204effee2e6e32f183440ba468e61
Agustin Castelli <rev0.c4chomax@gmail.com>
2024-04-10 14:51:59 -0300

    Ejercicio 2 LINQ

---
+++ Web/Controllers/Ej2LINQController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej2LINQController : ControllerBase
+    {
+        [HttpGet()]
+        public List<string> Get([FromQuery] List<string> queryString)
+        {
+            var query =
+                from s in queryString
+                where s.Length >= 5
+                select s.ToUpper();
+
+            return query.ToList();
+        }
+    }
+}
